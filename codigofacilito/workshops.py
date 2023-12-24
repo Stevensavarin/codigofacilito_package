0d7cc3cbe2771505f375c64a237e53152b92b899
steven_savarin <srssdesing@gmail.com>
2023-12-24 14:05:48 +0100

    Modulo logging

--- codigofacilito/workshops.py
+++ codigofacilito/workshops.py
@@ -1,8 +1,8 @@
 import requests
 
 def unreleased():
-    response = requests.get("https://codigofacilito.com/api/v2/workshops/unreleased")
+    response = requests.get("https://dummyjson.com/quotes")
 
     if response.status_code == 200:
         payload = response.json()
-        return payload["data"]
+        return payload["quotes"]
